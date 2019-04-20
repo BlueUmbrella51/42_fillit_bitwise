13080d64f31af585e513837f24e51db35134476a
Jesse Dunnink <jdunnink@f1r4s20.codam.nl>
2019-04-20 12:30:22 +0200

    changes to find_map

--- srcs/find_mapsize.c
+++ srcs/find_mapsize.c
@@ -12,27 +12,11 @@
 
 #include "fillit.h"
 
-// starting mapsize == sqrt(number of tetros * 4)
-
-static int		ft_is_prime(int c)
-{
-	int i;
-
-	i = 2;
-	if (c <= 1)
-		return (0);
-	while (i <= c / i)
-	{
-		if (c % i == 0)
-			return (0);
-		i++;
-	}
-	return (1);
-}
-
 size_t find_mapsize(size_t count)
 {
-    while (ft_is_prime(ft_sqrt(count * 4)) == 0)
+	if (count == 0)
+		return (0);
+    while (ft_sqrt(count * 4) == 0)
         count--;
     return (count * 4);
 }
