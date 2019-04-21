4188b8c672830412b7e9a430aebfd59d170ceda4
Laure Ravier <lravier@f1r4s16.codam.nl>
2019-04-21 13:45:40 +0200

    works for tetramino, not tested for maps yet (should work)

--- srcs/translate_for_map_increase.c
+++ srcs/translate_for_map_increase.c
@@ -15,51 +15,89 @@
 //01000 1	
 
 #include "fillit.h"
+size_t	get_row(size_t index, int total_new, int width_old)
+{
+	size_t test;
+	size_t row = 0;
 
+	test = total_new - width_old;
+	while (index <= test)
+	{
+		test -= width_old;
+		row++;
+	}
+	return (row);
+}
 void	ft_translate_for_map_increase(unsigned long long *map, size_t prev_size, size_t size, size_t num_tetros)
 {
-	unsigned long long new;
-	size_t diff;
+//both sizes in width, not total 
+	unsigned long long tmp;
+	unsigned long long res;
 	unsigned long long mask;
+	size_t diff;
 	size_t offset;
+	size_t row;
+	size_t j;
+	size_t i;
 
+	j = 0;
+	i = 1;
+	res = 0ULL;
 	diff = size - prev_size;
-	offset = (num_tetros * 4 - 1) * diff;
-	new = *map << diff;
-	mask = 1 << 0;
+	offset = prev_size;
+	tmp = *map;
+	tmp <<= (size * size) - (prev_size * prev_size);
+	mask = 1ULL;
 //skip all bits that are 0 and then skip first 1
-	while (offset != diff)
+	while (j < (num_tetros * 4 - 1))
 	{
 //skip until 'last' 1 is found
-		while (mask ^ new)
-			mask <<= 1;
+		while ((tmp & mask) == 0)
+		{
+			mask = (1ULL << i);
+			i++;
+		}
 //remove one that is found, then add it back later;
-		new ^= mask;
-		mask >>= offset;
-		new |= mask;
-		offset -= diff;
+		row = get_row(i, size * size, prev_size);
+		tmp ^= mask;
+		mask >>= offset * row;
+//		i -= offset * row;
+		res |= mask;
+		j++;
 	}
-	*map = new;
+	res |= tmp;
+	*map = res;
 }
 
 unsigned long long	tetro_to_ll(t_tetro *t)
 {
 	unsigned long long res;
+	unsigned long long tmp;
 	unsigned long long mask;
-	size_t offset;
+	int j;
+	size_t i;
+	size_t row;
 
-	res = t->tetro;
-	res <<= 12;
-	offset = 3;
-	mask = 1 << 0;
-	while (offset != 1)
+	j = 0;
+	row = 1;
+	res = 0ULL;
+	tmp = (unsigned long long)t->tetro;
+	tmp <<= 48ULL;
+	i = 1;
+	mask = 1ULL;
+	while (j < SIZE - 1)
 	{
-		while (mask ^ res)
-			mask <<= 1;
-		res ^= mask;
-		mask >>= offset;
+		while ((tmp & mask) == 0)
+		{
+			mask = (1ULL << i);
+			i++;
+		}
+		row = get_row(i, 64, 4);
+		tmp ^= mask;
+		mask >>= SIZE * row;
 		res |= mask;
-		offset -= 1;
+		j++;
 	}
+	res |= tmp;
 	return (res);
 }
