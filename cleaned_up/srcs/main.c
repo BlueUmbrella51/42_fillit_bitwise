3f63a5747cca53f91727e0537ec78b7329a5943b
Laure Ravier <lravier@f0r5s11.codam.nl>
2019-04-24 15:42:14 +0200

    main for testing and updated solver

--- cleaned_up/srcs/main.c
+++ cleaned_up/srcs/main.c
@@ -12,6 +12,41 @@
 
 #include "fillit.h"
 
+
+int main(void)
+{
+    t_list *tetros;
+    t_tetro tetro1;
+    t_tetro tetro2;
+    t_tetro tetro3;
+    unsigned long long map;
+    unsigned long long answer;
+    tetros = NULL;
+    map = 0;
+    answer = 0;
+    tetro1.tetro = 52224U;
+    tetro1.width = 2;
+    tetro1.height = 2;
+    tetro1.print = 'A';
+    tetro1.used = 0;
+    tetro2.tetro = 52224U;
+    tetro2.width = 2;
+    tetro2.height = 2;
+    tetro2.print = 'B';
+    tetro2.used = 0;
+    tetro3.tetro = 52224u;
+    tetro3.width = 2;
+    tetro3.height = 2;
+    tetro3.print = 'C';
+    tetro3.used = 0;
+    ft_lstpushfront(&tetro1, &tetros, sizeof(&tetro1));
+    ft_lstpushfront(&tetro2, &tetros, sizeof(&tetro2));
+    ft_lstpushfront(&tetro3, &tetros, sizeof(&tetro2));
+    map = 2315096499073056768U;
+    solver(&answer, 3, &tetros);
+//    print_tetro_long(answer, 64);
+}
+/*
 int	main(int argc, char *argv[])
 {
 	t_list *tetros;
@@ -35,3 +70,4 @@ int	main(int argc, char *argv[])
 	//call solver here
 	return (0);
 }
+*/
--- cleaned_up/srcs/map_solver.c
+++ cleaned_up/srcs/map_solver.c
@@ -1,5 +1,20 @@
 #include "fillit.h"
 
+int count_longs(unsigned long long *tetro)
+{
+    unsigned long long tmp;
+    int count;
+
+    count = 0;
+    tmp = *tetro;
+    while (tmp)
+    {
+        count += tmp & 1;
+        tmp >>= 1;
+    }
+    return (count);
+}
+
 static void	get_border(unsigned long long *border, size_t size)
 {
 	size_t i;
@@ -9,29 +24,34 @@ static void	get_border(unsigned long long *border, size_t size)
     total_size = size * size;
 	while (i < total_size)
 	{
-		*border |= 1 << i;
+		*border |= (1 << i);
 		i += size + 1;
 	}
 }
 
 int check_fit(unsigned long long *map, t_list *tetro, size_t size)
 {
-    int i;
+    size_t i;
     unsigned long long mask;
     unsigned long long t;
 
-    t = tetro_to_ll(((t_tetro *)((t_list *)tetro)->content)->tetro);
+    mask = 0ULL;
+    t = tetro_to_ll(((t_list *)tetro)->content);
     i = 0;
     get_border(&mask, size);
-    while (t ^ mask && i < size)
+    printf("BORDER: %llu\n", mask);
+    while (i < size)
     {
-        if (*map ^ t)
+        if ((*map & t) == 0)
         {
             ((t_tetro *)((t_list *)tetro)->content)->index = i;
-            *map = (*map ^ t);
+            *map |= t;
+            printf("MAP: %llu\nINDEX: %zu\n", *map, i);
             return (1);
         }
-        t = (1 >> i);
+        t >>= 1;
+        printf("TETRO: %llu\n", t);
+        i++;
     }
     return (0);
 }
@@ -41,31 +61,33 @@ int solve_map(unsigned long long *map, t_list **lst, size_t map_size, size_t num
     t_list *curr;
 
     curr = *lst;
-    if (num_ones(map) == num_tetros * SIZE);
+    if (count_longs(map) == (int)num_tetros * SIZE)
         return (1);
     while (curr)
     {
         if (check_fit(map, curr, map_size))
             if (solve_map(map, &curr->next, map_size, num_tetros))
                 return (1);
-        *map ^= tetro_to_ll(((t_tetro *)((t_list *)curr)->content)->tetro);
+        *map ^= tetro_to_ll(((t_list *)curr)->content);
         curr = curr->next;
+//        exit(0);
     }
     return (0);
 }
 
-int solver(size_t num_tetros, t_list **tetros)
+int solver(unsigned long long *map, size_t num_tetros, t_list **tetros)
 {
     size_t size;
-    size_t i;
-    unsigned long long map;
 
     size = min_mapsize(num_tetros);
-    while (i < 8)
+    printf("MAP_SIZE: %zu\nNUM_TETROS: %zu\n", size, num_tetros);
+    while (size < 8)
     {
-        if (solve_map(&map, tetros, size, num_tetros))
+        printf("in while\n");
+        if (solve_map(map, tetros, size, num_tetros))
             return (1);
         map = 0ULL;
-        i++;
+        size++;;
     }
+    return (0);
 }
