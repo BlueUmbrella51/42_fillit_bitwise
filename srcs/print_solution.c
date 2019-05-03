32ae257548b0db253b78e39f426330957323f53c
Jesse Dunnink <jdunnink@f0r4s13.codam.nl>
2019-05-03 17:00:23 +0200

    print_solution

--- srcs/print_solution.c
+++ srcs/print_solution.c
@@ -6,103 +6,103 @@
 /*   By: jdunnink <marvin@codam.nl>                   +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/05/03 15:38:05 by jdunnink      #+#    #+#                 */
-/*   Updated: 2019/05/03 15:43:05 by jdunnink      ########   odam.nl         */
+/*   Updated: 2019/05/03 16:59:42 by jdunnink      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 
-static  char    **initialize_array(size_t size)
+static	char	**initialize_array(size_t size)
 {
-    char **res;
-    size_t i;
+	char    **res;
+	size_t  i;
 
-    i = 0;
-    res = (char **)malloc(sizeof(char *) * size);
-    if (!res)
+	i = 0;
+	res = (char **)malloc(sizeof(char *) * size);
+	if (!res)
 		return (NULL);
-    while (i < size)
-    {
+	while (i < size)
+	{
 		res[i] = ft_strnew(size);
 		i++;
-    }
-    return (res);
+	}
+	return (res);
 }
 
-static  void    fill_array(char **res, size_t mapsize)
+static	void	fill_array(char **res, size_t mapsize)
 {
-    size_t i;
-    size_t j;
+	size_t  i;
+	size_t  j;
 
-    i = 0;
-    j = 0;
-    while (j < mapsize)
-    {
-        i = 0;
-        while (i < mapsize)
-        {
-            res[j][i] = '.';
-            i++;
-        }
-        j++;
-    }
+	i = 0;
+	j = 0;
+	while (j < mapsize)
+	{
+		i = 0;
+		while (i < mapsize)
+		{
+			res[j][i] = '.';
+			i++;
+		}
+		j++;
+	}
 }
 
-static  void    put_map(char **res, size_t mapsize)
+static	void	put_map(char **res, size_t mapsize)
 {
-    size_t i;
-    size_t j;
+	size_t  i;
+	size_t  j;
 
-    i = 0;
-    j = 0;
-    while (j < mapsize)
-    {
-        i = 0;
-        while (i < mapsize)
-        {
-            ft_putchar(res[j][i]);
-            i++;
-        }
-        ft_putchar('\n');
-        j++;
-    }
+	i = 0;
+	j = 0;
+	while (j < mapsize)
+	{
+		i = 0;
+		while (i < mapsize)
+		{
+			ft_putchar(res[j][i]);
+			i++;
+		}
+		ft_putchar('\n');
+		j++;
+	}
 }
 
-static  void    place_tetro(t_tetro *t, char **map)
+static	void	place_tetro(t_tetro *t, char **map)
 {
-    size_t i;
-    int x;
-    int y;
-    uint16_t mask;
+	size_t		i;
+	int			x;
+    int			y;
+    uint16_t	mask;
 
-    mask = (1U << 15);
-    i = 0;
-    x = 0;
-    y = 0;
-    while (mask)
-    {
-        if (mask & t->fpt)
-        {
-            itocoor(&x, &y, i, 4);
-            map[y + t->y][x + t->x] = t->print;
-        }
-        i++;
-        mask >>= 1;
-    }
+	mask = (1U << 15);
+	i = 0;
+	x = 0;
+	y = 0;
+	while (mask)
+	{
+		if (mask & t->fpt)
+		{
+			itocoor(&x, &y, i, 4);
+			map[y + t->y][x + t->x] = t->print;
+		}
+		i++;
+		mask >>= 1;
+	}
 }
 
-void            print_solution (t_list **tetros, size_t mapsize)
+void			print_solution (t_list **tetros, size_t mapsize)
 {
-    char **res;
-    t_list *curr;
-
-    res = initialize_array(mapsize);
-    fill_array(res, mapsize);
-    curr = *tetros;
-    while(curr)
-    {
-        place_tetro(curr->content, res);
-        curr = curr->next;
-    }
-    put_map(res, mapsize);
+    char		**res;
+    t_list		*curr;
+	
+	res = initialize_array(mapsize);
+	fill_array(res, mapsize);
+	curr = *tetros;
+	while (curr)
+	{
+		place_tetro(curr->content, res);
+		curr = curr->next;
+	}
+	put_map(res, mapsize);
 }
