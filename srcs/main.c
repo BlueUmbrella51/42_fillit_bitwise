f03815a3b8ea1f0f88865d457a357f6f859084ac
Jesse Dunnink <jdunnink@f0r4s13.codam.nl>
2019-05-03 17:34:07 +0200

    main

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: jdunnink <marvin@codam.nl>                   +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/05/03 15:33:32 by jdunnink      #+#    #+#                 */
-/*   Updated: 2019/05/03 15:34:51 by jdunnink      ########   odam.nl         */
+/*   Updated: 2019/05/03 17:33:43 by jdunnink      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,26 +14,26 @@
 
 int main(int argc, char *argv[])
 {
-    uint16_t *map;
-    t_list *tetros;
-    size_t mapsize;
-    int fd;
-    int res;
-    size_t count;
+	uint16_t *map;
+	t_list *tetros;
+	size_t mapsize;
+	int fd;
+	int res;
+	size_t count;
 
-    if (argc != 2)
-      return (ft_error("usage: source_file"));
-    fd = open(argv[1], O_RDONLY);
-    if (fd < 0)
-		  return(ft_error("error"));
-    count = 0;
-    map = (uint16_t *)malloc(sizeof(uint16_t) * 16);
-    ft_bzero(map, sizeof(uint16_t) * 16);
-	  res = read_input(fd, &tetros, &count);
-    if (res < 0)
-      return (res);
-    mapsize = checker(map, &tetros, count);
-    mapsize = solver(map, &tetros, mapsize);
-    print_solution(&tetros, mapsize);
+	if (argc != 2)
+		return (ft_error("usage: source_file"));
+	fd = open(argv[1], O_RDONLY);
+	if (fd < 0)
+		return(ft_error("error"));
+	count = 0;
+	map = (uint16_t *)malloc(sizeof(uint16_t) * 16);
+	ft_bzero(map, sizeof(uint16_t) * 16);
+	res = read_input(fd, &tetros, &count);
+	if (res < 0)
+		return (res);
+	mapsize = checker(map, &tetros, count);
+	mapsize = solver(map, &tetros, mapsize);
+	print_solution(&tetros, mapsize);
 	return (0);
 }
