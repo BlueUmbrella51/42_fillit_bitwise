f03566b06e2c7aa66111c8b0b61fda60a58a87fa
Laure Ravier <lravier@f0r4s14.codam.nl>
2019-05-03 18:21:01 +0200

    count ones norm

--- srcs/count_ones.c
+++ srcs/count_ones.c
@@ -1,9 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   count_ones.c                                       :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: lravier <marvin@codam.nl>                    +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2019/05/03 18:18:00 by lravier       #+#    #+#                 */
+/*   Updated: 2019/05/03 18:18:44 by lravier       ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "fillit.h"
 
-int	count_ones(uint16_t *tetro)
+int		count_ones(uint16_t *tetro)
 {
-	uint16_t tmp;
-	int count;
+	uint16_t	tmp;
+	int			count;
 
 	count = 0;
 	tmp = *tetro;
