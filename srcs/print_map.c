fb0e697e2f301855c74a7a19dccaa34b48d23dc6
Jesse Dunnink <jdunnink@f0r4s13.codam.nl>
2019-05-03 18:14:07 +0200

    headers

--- srcs/print_map.c
+++ srcs/print_map.c
@@ -6,13 +6,13 @@
 /*   By: jdunnink <marvin@codam.nl>                   +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/05/03 15:46:05 by jdunnink      #+#    #+#                 */
-/*   Updated: 2019/05/03 18:03:26 by jdunnink      ########   odam.nl         */
+/*   Updated: 2019/05/03 18:11:34 by jdunnink      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 
-void    print_map(uint16_t **map)
+void	print_map(uint16_t **map)
 {
 	int i;
 
