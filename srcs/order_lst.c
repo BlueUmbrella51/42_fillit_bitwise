d770b05d62327a32a5a4e104a6fd3e5029b43b0a
Jesse Dunnink <jdunnink@f0r4s13.codam.nl>
2019-05-03 17:28:35 +0200

    order list

--- srcs/order_lst.c
+++ srcs/order_lst.c
@@ -1,39 +1,39 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   utilities1.c                                       :+:    :+:            */
+/*   order_lst.c                                        :+:    :+:            */
 /*                                                     +:+                    */
 /*   By: jdunnink <marvin@codam.nl>                   +#+                     */
 /*                                                   +#+                      */
-/*   Created: 2019/05/03 15:46:05 by jdunnink      #+#    #+#                 */
-/*   Updated: 2019/05/03 15:46:15 by jdunnink      ########   odam.nl         */
+/*   Created: 2019/05/03 17:26:14 by jdunnink      #+#    #+#                 */
+/*   Updated: 2019/05/03 17:27:07 by jdunnink      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 
-void    list_sort(t_list *tetros)
+void	list_sort(t_list *tetros)
 {
-    t_list *iter;
-    t_list *trail;
-    t_list *tmp;
+	t_list *iter;
+	t_list *trail;
+	t_list *tmp;
 
-    iter = tetros->next;
-    trail = tetros;
-    while (iter)
-    {
-        if (((t_tetro*)iter->content)->order < (((t_tetro*)trail->content)->order))
-        {
-            tmp = iter->content;
-            iter->content = trail->content;
-            trail->content = tmp;
-            trail = tetros;
-            iter = tetros->next;
-        }
-        else
-        {
-            iter = iter->next;
-            trail = trail->next;
-        }
-    }
+	iter = tetros->next;
+	trail = tetros;
+	while (iter)
+	{
+		if (((t_tetro*)iter->content)->order < (((t_tetro*)trail->content)->order))
+ 		{
+			tmp = iter->content;
+			iter->content = trail->content;
+			trail->content = tmp;
+			trail = tetros;
+			iter = tetros->next;
+		}
+		else
+		{
+			iter = iter->next;
+			trail = trail->next;
+		}
+	}
 }
