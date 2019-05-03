e1b12f36ead00a38525c1d27910a528c0d293092
Jesse Dunnink <jdunnink@f0r4s13.codam.nl>
2019-05-03 17:17:27 +0200

    read tetro

--- srcs/read_tetro.c
+++ srcs/read_tetro.c
@@ -1,24 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   read_tetro.c                                       :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: jdunnink <marvin@codam.nl>                   +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2019/05/03 17:14:17 by jdunnink      #+#    #+#                 */
+/*   Updated: 2019/05/03 17:16:23 by jdunnink      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "fillit.h"
 
-static void        to_bits(char *s, uint16_t *dst, int iter, char f)
+static	void	to_bits(char *s, uint16_t *dst, int iter, char f)
 {
-    int i;
-    uint16_t res;
-    int len;
+	int			i;
+	uint16_t	res;
+	int			len;
 
-    len = ft_strlen(s);
-    i = 0;
-    res = 0;
-    while (s[i] != '\0')
-    {
-        if (s[i] == f)
-            res |= 1U << (len - (i + 1));
-        i++;
-    }
+	len = ft_strlen(s);
+	i = 0;
+	res = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] == f)
+			res |= 1U << (len - (i + 1));
+		i++;
+	}
 	*dst |= res << (len * (len - 1) - (len * iter));
 }
 
-static int		valid_characters(char *line, char f, char e)
+static	int		valid_characters(char *line, char f, char e)
 {
 	size_t i;
 
@@ -32,24 +44,24 @@ static int		valid_characters(char *line, char f, char e)
 	return (1);
 }
 
-int  read_tetromino(int *lr, int fd, char **line, unsigned short *dst)
+int				read_tetromino(int *lr, int fd, char **line, unsigned short *dst)
 {
-    int i;
+	int i;
 
-    i = 0;
-    while (i < SIZE)
-    {
-        *lr = get_next_line(fd, line);
-        if (*lr == 0)
-            return (ft_error("error"));
-        if (*lr < 0)
-            return(ft_error("error"));
-        if (!(valid_characters(*line, '#', '.')))
-            return(ft_error("error"));
-        if (!(ft_strlen(*line) == 4))
-            return(ft_error("error"));
-        to_bits(*line, dst, i, '#');
-        i++;
-    }
-    return (1);
+	i = 0;
+	while (i < SIZE)
+	{
+		*lr = get_next_line(fd, line);
+		if (*lr == 0)
+			return (ft_error("error"));
+		if (*lr < 0)
+			return (ft_error("error"));
+		if (!(valid_characters(*line, '#', '.')))
+			return (ft_error("error"));
+		if (!(ft_strlen(*line) == 4))
+			return(ft_error("error"));
+		to_bits(*line, dst, i, '#');
+		i++;
+	}
+	return (1);
 }
