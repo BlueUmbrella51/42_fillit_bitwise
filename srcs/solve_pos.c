74f4900364082098c802e6f5245deac82ae10bc2
Jesse Dunnink <jdunnink@f0r4s13.codam.nl>
2019-05-04 14:24:25 +0200

    norme

--- srcs/solve_pos.c
+++ srcs/solve_pos.c
@@ -6,64 +6,64 @@
 /*   By: jdunnink <marvin@codam.nl>                   +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/05/04 13:48:05 by jdunnink      #+#    #+#                 */
-/*   Updated: 2019/05/04 14:09:39 by jdunnink      ########   odam.nl         */
+/*   Updated: 2019/05/04 14:21:05 by jdunnink      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 
-static  int     check_fit(uint16_t *map, t_tetro *tetro)
+static	int		check_fit(uint16_t *map, t_tetro *tetro)
 {
-    if((*(uint64_t *)(map + tetro->y) & (tetro->tetro >> tetro->x)) == 0)
-        return (1);
-    return (0);
+	if ((*(uint64_t *)(map + tetro->y) & (tetro->tetro >> tetro->x)) == 0)
+		return (1);
+	return (0);
 }
 
-static  int     same_rest(t_list *curr, t_list *rest)
+static	int		same_rest(t_list *curr, t_list *rest)
 {
-    t_list  *r;
-    t_tetro *rc;
-    t_tetro *ch;
+	t_list	*r;
+	t_tetro *rc;
+	t_tetro *ch;
 
-    r = rest;
-    ch = curr->content;
-    if (!r)
-        return (0);
-    while (r)
-    {
-        rc = r->content;
-        if (ch->fpt != rc->fpt)
-            return (0);
-        r = r->next;
-    }
-    return (1);
+	r = rest;
+	ch = curr->content;
+	if (!r)
+		return (0);
+	while (r)
+	{
+		rc = r->content;
+		if (ch->fpt != rc->fpt)
+			return (0);
+		r = r->next;
+	}
+	return (1);
 }
 
-int     solve_pos(t_list *curr, t_tetro *tetro, size_t map_size, uint16_t *map)
+int				solve_pos(t_list *lst, t_tetro *tro, size_t size, uint16_t *map)
 {
-    while (tetro->y <= map_size - tetro->height)
-    {
-        tetro->x = 0;
-        while (tetro->x <= map_size - tetro->width)
-        {
-            if (check_fit(map, tetro))
+	while (tro->y <= size - tro->height)
+	{
+		tro->x = 0;
+		while (tro->x <= size - tro->width)
+		{
+			if (check_fit(map, tro))
 			{
-				toggle_tetro(map, tetro);
-				if (solve_map(map, &curr->next, map_size))
+				toggle_tetro(map, tro);
+				if (solve_map(map, &lst->next, size))
 					return (1);
-				toggle_tetro(map, tetro);
-				if (same_rest(curr, curr->next))
+				toggle_tetro(map, tro);
+				if (same_rest(lst, lst->next))
 				{
-					tetro->x = 0;
-					tetro->y = 0;
+					tro->x = 0;
+					tro->y = 0;
 					return (0);
 				}
 			}
-			tetro->x++;
+			tro->x++;
 		}
-		tetro->y++;
+		tro->y++;
 	}
-	tetro->x = 0;
-	tetro->y = 0;
+	tro->x = 0;
+	tro->y = 0;
 	return (0);
 }
