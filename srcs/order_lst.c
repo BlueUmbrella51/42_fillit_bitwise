00b27f17e92dcf200bf626c8bbebd42217a762e2
Jesse Dunnink <jdunnink@f0r4s13.codam.nl>
2019-05-04 12:45:12 +0200

    order_lst.c

--- srcs/order_lst.c
+++ srcs/order_lst.c
@@ -6,7 +6,7 @@
 /*   By: jdunnink <marvin@codam.nl>                   +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/05/03 17:26:14 by jdunnink      #+#    #+#                 */
-/*   Updated: 2019/05/03 17:27:07 by jdunnink      ########   odam.nl         */
+/*   Updated: 2019/05/04 12:44:47 by jdunnink      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ void	list_sort(t_list *tetros)
 	while (iter)
 	{
 		if (((t_tetro*)iter->content)->order < (((t_tetro*)trail->content)->order))
- 		{
+		{
 			tmp = iter->content;
 			iter->content = trail->content;
 			trail->content = tmp;
