4b7e64831ff85ee3f1df956ebdbfa9b962005d8c
Jesse Dunnink <jdunnink@f0r4s13.codam.nl>
2019-05-04 12:11:29 +0200

    main.c

--- srcs/main.c
+++ srcs/main.c
@@ -5,27 +5,27 @@
 /*                                                     +:+                    */
 /*   By: jdunnink <marvin@codam.nl>                   +#+                     */
 /*                                                   +#+                      */
-/*   Created: 2019/05/03 15:33:32 by jdunnink      #+#    #+#                 */
-/*   Updated: 2019/05/03 17:33:43 by jdunnink      ########   odam.nl         */
+/*   Created: 2019/05/04 12:10:09 by jdunnink      #+#    #+#                 */
+/*   Updated: 2019/05/04 12:11:01 by jdunnink      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 
-int main(int argc, char *argv[])
+int	main(int argc, char *argv[])
 {
-	uint16_t *map;
-	t_list *tetros;
-	size_t mapsize;
-	int fd;
-	int res;
-	size_t count;
+	uint16_t	*map;
+	t_list		*tetros;
+	size_t		mapsize;
+	int			fd;
+	int			res;
+	size_t		count;
 
 	if (argc != 2)
 		return (ft_error("usage: source_file"));
 	fd = open(argv[1], O_RDONLY);
 	if (fd < 0)
-		return(ft_error("error"));
+		return (ft_error("error"));
 	count = 0;
 	map = (uint16_t *)malloc(sizeof(uint16_t) * 16);
 	ft_bzero(map, sizeof(uint16_t) * 16);
