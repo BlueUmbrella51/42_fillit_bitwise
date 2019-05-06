2fb4aa7b1d7b29f2595ad8f8dcb9642d5a302e7d
Jesse Dunnink <jdunnink@f0r4s13.codam.nl>
2019-05-06 10:41:56 +0200

    clean up

--- srcs/min_mapsize.c
+++ srcs/min_mapsize.c
@@ -1,6 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   min_mapsize.c                                      :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: jdunnink <marvin@codam.nl>                   +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2019/05/06 10:39:24 by jdunnink      #+#    #+#                 */
+/*   Updated: 2019/05/06 10:39:35 by jdunnink      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "fillit.h"
 
-size_t			min_mapsize(size_t num_tetros)
+size_t	min_mapsize(size_t num_tetros)
 {
 	size_t i;
 
